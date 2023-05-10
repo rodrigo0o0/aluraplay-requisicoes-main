4d9744222092161c915f1c81baed3f330f9952c7
rodrigo0o0 <sandeski47@gmail.com>
2023-05-10 04:23:39 -0300

    Implementado o método post inserindo no JSON o novo vídeo e redirecionando para a página de enviado

---
+++ js/criarVideo.js
@@ -0,0 +1,19 @@
+import { conectaApi } from "./conectaApi.js";
+
+const formEnvio = document.querySelector('[data-formulario]');
+formEnvio.addEventListener('submit',(event) => criarVideo(event));
+
+
+
+async function criarVideo(event) {
+    event.preventDefault();
+    var url = document.querySelector('[data-url]').value;
+    var titulo = document.querySelector('[data-titulo]').value;
+    var imagem = document.querySelector('[data-imagem]').value;
+    var descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectaApi.criaVideo(titulo,descricao,url,imagem);
+
+    window.location.href = '../pages/envio-concluido.html';
+}
+
